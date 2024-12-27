e3e3e4f9b3c83a2cbd2b5a7ea233dbdbe9851572
vitor poerschke <vitordrakomanico@gmail.com>
2024-12-27 16:31:41 -0300

    primeiro commit

---
+++ ticket-manager/src/main/java/com/ms_ticket_manager/ticket_manager/TicketManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ms_ticket_manager.ticket_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketManagerApplication.class, args);
+	}
+
+}
