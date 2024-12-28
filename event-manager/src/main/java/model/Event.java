a40e5908287c597d7117acd9a916b80af58b977b
vitor poerschke <vitordrakomanico@gmail.com>
2024-12-27 23:39:54 -0300

    feat: package model and class Event

---
+++ event-manager/src/main/java/model/Event.java
@@ -0,0 +1,19 @@
+package model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "events")
+public class Event {
+    @Id
+    private String id;
+    private String eventName;
+    private String dateTime;
+    private String cep;
+    private String logradouro;
+    private String bairro;
+    private String cidade;
+    private String uf;
+}
