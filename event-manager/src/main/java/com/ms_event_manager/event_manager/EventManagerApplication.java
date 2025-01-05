8c35615078fd9cb07df0b1fd02270e53f2dcc4ff
vitor poerschke <vitordrakomanico@gmail.com>
2025-01-05 16:44:52 -0300

    fix: EventManagerApllication annotation FeeignClients

--- event-manager/src/main/java/com/ms_event_manager/event_manager/EventManagerApplication.java
+++ event-manager/src/main/java/com/ms_event_manager/event_manager/EventManagerApplication.java
@@ -2,8 +2,10 @@ package com.ms_event_manager.event_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class EventManagerApplication {
 
 	public static void main(String[] args) {
