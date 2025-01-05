49d1c5b460b22b143d9d9772afbb6ac7158eeea2
vitor poerschke <vitordrakomanico@gmail.com>
2025-01-05 16:42:08 -0300

    fix: class ViaCepClient for interface

--- event-manager/src/main/java/feign/ViaCepClient.java
+++ event-manager/src/main/java/feign/ViaCepClient.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.Map;
 
 @FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
-public class ViaCepClient {
+
+public interface ViaCepClient {
     @GetMapping("/{cep}/json")
     Map<String, String> getCepDetails(@PathVariable("cep") String cep);
-    //tenho que corrigir esta classe, está dando erro!//
 }
