a3ffdf18ffc668d25146d4c7d8a097cad4c74fa6
vitor poerschke <vitordrakomanico@gmail.com>
2025-01-12 16:06:27 -0300

    feat: created class global exceptions handler

---
+++ event-manager/src/main/java/com/ms_event_manager/event_manager/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.ms_event_manager.event_manager.exceptions;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
