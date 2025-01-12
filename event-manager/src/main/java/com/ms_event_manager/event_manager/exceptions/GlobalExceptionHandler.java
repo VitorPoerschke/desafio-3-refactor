ccbd2440e9bf6daf8cbdad8aae4104d1a4d5ff54
vitor poerschke <vitordrakomanico@gmail.com>
2025-01-12 16:16:09 -0300

    feat: function created -> INTERNAL_SERVER_ERROR

--- event-manager/src/main/java/com/ms_event_manager/event_manager/exceptions/GlobalExceptionHandler.java
+++ event-manager/src/main/java/com/ms_event_manager/event_manager/exceptions/GlobalExceptionHandler.java
@@ -25,4 +25,9 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobalException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro no servidor.");
+    }
+
 }
