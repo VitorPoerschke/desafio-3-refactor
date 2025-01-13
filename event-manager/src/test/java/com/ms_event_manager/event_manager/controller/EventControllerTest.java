1f4c7baff445647615ef75095ed85239d264ac59
vitor poerschke <vitordrakomanico@gmail.com>
2025-01-12 22:47:24 -0300

    test: testCreateEvent_Success

---
+++ event-manager/src/test/java/com/ms_event_manager/event_manager/controller/EventControllerTest.java
@@ -0,0 +1,49 @@
+package com.ms_event_manager.event_manager.controller;
+
+import com.ms_event_manager.event_manager.dto.EventDTO;
+import com.ms_event_manager.event_manager.model.Event;
+import com.ms_event_manager.event_manager.service.EventService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class EventControllerTest {
+
+    @InjectMocks
+    private EventController controller;
+
+    @Mock
+    private EventService service;
+
+    @Test
+    void testCreateEvent_Success() {
+        EventDTO dto = new EventDTO();
+        dto.setEventName("Evento Teste");
+        dto.setDateTime("2025-01-12T15:00:00");
+        dto.setCep("12345678");
+        Event event = new Event();
+        event.setId("1");
+        event.setEventName("Evento Teste");
+        event.setDateTime("2025-01-12T15:00:00");
+        event.setCep("12345678");
+        event.setLogradouro("Rua X");
+        event.setBairro("Centro");
+        event.setCidade("São Paulo");
+        event.setUf("SP");
+        when(service.createEvent(dto)).thenReturn(event);
+        ResponseEntity<Event> response = controller.createEvent(dto);
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(event, response.getBody());
+    }
+
+}
