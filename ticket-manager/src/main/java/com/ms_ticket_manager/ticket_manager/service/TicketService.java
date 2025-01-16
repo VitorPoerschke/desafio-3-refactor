07ad825d95c2a2557e40fcf83980bee07a5ae1cb
vitor poerschke <vitordrakomanico@gmail.com>
2025-01-16 14:23:40 -0300

    feat: service createTicket

--- ticket-manager/src/main/java/com/ms_ticket_manager/ticket_manager/service/TicketService.java
+++ ticket-manager/src/main/java/com/ms_ticket_manager/ticket_manager/service/TicketService.java
@@ -1,5 +1,6 @@
 package com.ms_ticket_manager.ticket_manager.service;
 
+import com.ms_ticket_manager.ticket_manager.model.Ticket;
 import com.ms_ticket_manager.ticket_manager.repository.TicketRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,9 @@ import org.springframework.stereotype.Service;
 public class TicketService {
 
     private final TicketRepository repository;
+
+    public Ticket createTicket(Ticket ticket) {
+        return repository.save(ticket);
+    }
 
 }
