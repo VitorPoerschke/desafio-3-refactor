a41fb7f4fe73dfacd2067cba09595d059e27d4a2
vitor poerschke <vitordrakomanico@gmail.com>
2025-01-16 15:13:56 -0300

    feat: controller -> getTicketById

--- ticket-manager/src/main/java/com/ms_ticket_manager/ticket_manager/controller/TicketController.java
+++ ticket-manager/src/main/java/com/ms_ticket_manager/ticket_manager/controller/TicketController.java
@@ -4,10 +4,7 @@ import com.ms_ticket_manager.ticket_manager.model.Ticket;
 import com.ms_ticket_manager.ticket_manager.service.TicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/tickets")
@@ -22,5 +19,11 @@ public class TicketController {
         return ResponseEntity.ok(createdTicket);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Ticket> getTicketById(@PathVariable String id) {
+        return service.getTicketById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 
 }
