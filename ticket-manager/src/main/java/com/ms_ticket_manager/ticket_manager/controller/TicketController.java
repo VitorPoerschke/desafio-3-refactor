910dd6448de01bb0e2be02d0c3b8c174a1eec792
vitor poerschke <vitordrakomanico@gmail.com>
2025-01-16 15:13:03 -0300

    feat: controller -> createTicket

--- ticket-manager/src/main/java/com/ms_ticket_manager/ticket_manager/controller/TicketController.java
+++ ticket-manager/src/main/java/com/ms_ticket_manager/ticket_manager/controller/TicketController.java
@@ -1,4 +1,26 @@
 package com.ms_ticket_manager.ticket_manager.controller;
 
+import com.ms_ticket_manager.ticket_manager.model.Ticket;
+import com.ms_ticket_manager.ticket_manager.service.TicketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/tickets")
+@RequiredArgsConstructor
 public class TicketController {
+
+    private final TicketService service;
+
+    @PostMapping
+    public ResponseEntity<Ticket> createTicket(@RequestBody Ticket ticket) {
+        Ticket createdTicket = service.createTicket(ticket);
+        return ResponseEntity.ok(createdTicket);
+    }
+
+
 }
