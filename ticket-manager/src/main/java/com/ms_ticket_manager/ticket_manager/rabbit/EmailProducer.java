5863b0869f2bfb411233c9025bf6e6255862f978
vitor poerschke <vitordrakomanico@gmail.com>
2025-01-26 17:01:42 -0300

    feat: EmailProducer

--- ticket-manager/src/main/java/com/ms_ticket_manager/ticket_manager/rabbit/EmailProducer.java
+++ ticket-manager/src/main/java/com/ms_ticket_manager/ticket_manager/rabbit/EmailProducer.java
@@ -1,4 +1,27 @@
 package com.ms_ticket_manager.ticket_manager.rabbit;
 
+import com.ms_ticket_manager.ticket_manager.model.EmailMessage;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${rabbitmq.routingkey}")
+    private String routingKey;
+
+    public EmailProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendEmailMessage(EmailMessage emailMessage) {
+        rabbitTemplate.convertAndSend(exchange, routingKey, emailMessage);
+        System.out.println("Mensagem enviada para a fila: " + emailMessage);
+    }
 }
